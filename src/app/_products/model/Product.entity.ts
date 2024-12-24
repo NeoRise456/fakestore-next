055ad8db257e2d5069a007cc2569490f2dde0b1e
NeoRise <neorise456@gmail.com>
2024-12-24 16:56:20 -0500

    feat(product): add product entities.

---
+++ src/app/_products/model/Product.entity.ts
@@ -0,0 +1,21 @@
+import {Rating} from "@/app/_products/model/Rating.entity";
+
+export class Product {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: Rating;
+
+    constructor(id: number, title: string, price: number, description: string, category: string, image: string, rating: Rating) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.image = image;
+        this.rating = rating;
+    }
+}
