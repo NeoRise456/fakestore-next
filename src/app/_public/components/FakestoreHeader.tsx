700ce9adc5d72edc86fb7376be96d3df6ee40e36
NeoRise <neorise456@gmail.com>
2024-12-24 16:55:54 -0500

    feat: add header.

---
+++ src/app/_public/components/FakestoreHeader.tsx
@@ -0,0 +1,12 @@
+
+export default function FakestoreHeader() {
+    return (
+        <header>
+            <div className="border-b px-4 py-2 ">
+                <div className="container ">
+                    <span className="text-lg font-bold text-primary"> Fakestore </span>
+                </div>
+            </div>
+        </header>
+    )
+}
